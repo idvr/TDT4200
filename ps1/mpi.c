3122beebd558bcdb53fbc1bef642d34596b2976e
x10an14 <x10an14@hotmail.com>
2014-08-28 12:04:20 +0200

    Finished mpi, need to remove comments.

--- ps1/mpi.c
+++ ps1/mpi.c
@@ -4,46 +4,49 @@
 
 #define TAG 1047
 
-void send(int rank, int *msg, int direction){
+void send1(int rank, int *msg, int direction){
     *msg += 1;
+    puts( "send1ing messages" );
     MPI_Send(msg, 1, MPI_INT, rank+direction, TAG, MPI_COMM_WORLD);
     printf("Rank %d sent %d\n", rank, *msg);
 }
 
-void recv(int rank, int *msg, int direction, MPI_Status status){
+void recv1(int rank, int *msg, int direction, MPI_Status status){
     MPI_Recv(msg, 1, MPI_INT, rank+direction, TAG, MPI_COMM_WORLD, &status);
     printf("Rank %d received %d\n", rank, *msg);
 }
 
 int main(int argc, char** argv) {
-    int size, rank, msg;
     MPI_Status status;
+    int size, rank, msg;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if(rank > 0 && rank < (size-1)){ //If rank is not zero or size-1 (AKA first or last)
-        //Send the integer "Forwards"
-        recv(rank, &msg, -1, status);
-        send(rank, &msg, 1);
-
-        //Send the integer "Backwards"
-        recv(rank, &msg, 1, status);
-        send(rank, &msg, -1);
-    } else if(rank == 0){
+    if (rank == 0){
+        puts( "Rank 0" );
         //Start the transmission of the integer through the ranks/nodes
         msg = -1;
-        send(rank, &msg, 1);
+        send1(rank, &msg, 1);
 
         //Receive the integer after having been through all the other nodes but one twice.
-        recv(rank, &msg, 1, status);
-    } else{ //If rank == size -1, aka final/highest rank/node.
-        //Receive the integer "msg"
-        recv(rank, &msg, -1, status);
+        recv1(rank, &msg, 1, status);
+    } else if (rank < (size-1)){ //If rank is not zero or size-1 (AKA first or last)
+        //send1 the integer "Forwards"
+        puts( "Rank all" );
+        recv1(rank, &msg, -1, status);
+        send1(rank, &msg, 1);
 
-        //Increment and send integer back.
-        send(rank, &msg, -1);
+        //send1 the integer "Backwards"
+        recv1(rank, &msg, 1, status);
+        send1(rank, &msg, -1);
+    } else{//If rank == size -1, aka final/highest rank/node.
+        //Receive the integer "msg"
+        puts( "rank size" );
+        recv1(rank, &msg, -1, status);
+        //Increment and send1 integer back.
+        send1(rank, &msg, -1);
     }
 
     MPI_Finalize();
