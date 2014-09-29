569bb435b54949403dfb5a64a079a0eb3099695d
x10an14 <x10an14@hotmail.com>
2014-09-29 18:02:41 +0200

    Finished OpenMP implementation.
    Pthreads left...

--- ps4/histogram_omp.c
+++ ps4/histogram_omp.c
@@ -9,31 +9,33 @@ const int image_size = 512*512;
 const int color_depth = 255;
 
 int main(int argc, char** argv){
-
     if(argc != 3){
         printf("Useage: %s image n_threads\n", argv[0]);
         exit(-1);
     }
+
     int n_threads = atoi(argv[2]);
+    omp_set_num_threads(n_threads);
 
     unsigned char* image = read_bmp(argv[1]);
-    unsigned char* output_image = malloc(sizeof(unsigned char) * image_size);
+    unsigned char* output_image = malloc(sizeof(unsigned char)*image_size);
 
 
     int* histogram = (int*)calloc(sizeof(int), color_depth);
-    for(int i = 0; i < image_size; i++){
+    #pragma omp parallel for shared(histogram)
+     for(int i = 0; i < image_size; i++){
         histogram[image[i]]++;
     }
 
-
     float* transfer_function = (float*)calloc(sizeof(float), color_depth);
+    #pragma omp parallel for shared(transfer_function)
     for(int i = 0; i < color_depth; i++){
         for(int j = 0; j < i+1; j++){
             transfer_function[i] += color_depth*((float)histogram[j])/(image_size);
         }
     }
 
-
+    #pragma omp parallel for
     for(int i = 0; i < image_size; i++){
         output_image[i] = transfer_function[image[i]];
     }
