cf1310f460193c6af2ff467c62101f85726b16c5
x10an14 <x10an14@hotmail.com>
2014-10-16 17:57:40 +0200

    First commit of gpu.

--- ps5/raycast_gpu.cu
+++ ps5/raycast_gpu.cu
@@ -1,18 +1,10 @@
 #include "raycast.h"
 
-__global__ void raycast_kernel(unsigned char* data, unsigned char* image, unsigned char* region){
-
-}
-
-__global__ void raycast_kernel_texture(unsigned char* image){
-
-}
-
-unsigned char* raycast_gpu(unsigned char* data, unsigned char* region){
+unsigned char* grow_region_gpu(unsigned char* data){
     return NULL;
 }
 
-unsigned char* raycast_gpu_texture(unsigned char* data, unsigned char* region){
+unsigned char* raycast_gpu(unsigned char* data, unsigned char* region){
     return NULL;
 }
 
@@ -20,18 +12,9 @@ __global__ void region_grow_kernel(unsigned char* data, unsigned char* region, i
 
 }
 
-__global__ void region_grow_kernel_shared(unsigned char* data, unsigned char* region, int* finished){
+__global__ void raycast_kernel(unsigned char* data, unsigned char* image, unsigned char* region){
 
 }
-
-unsigned char* grow_region_gpu(unsigned char* data){
-    return NULL;
-}
-
-unsigned char* grow_region_gpu_shared(unsigned char* data){
-    return NULL;
-}
-
 
 int main(int argc, char** argv){
 
@@ -41,10 +24,18 @@ int main(int argc, char** argv){
 
     unsigned char* data = create_data();
 
-    /* //Serial version
-    unsigned char* region = grow_region_serial(data);
+    printf("Done creating data\n");
 
-    unsigned char* image = raycast_serial(data, region);*/
+    unsigned char* region = grow_region_gpu(data);
 
-    //write_bmp(image, IMAGE_DIM, IMAGE_DIM, "raycast_gpu_out.bmp");
+    printf("Done creating region\n");
+
+    unsigned char* image = raycast_gpu(data, region);
+
+    printf("Done creating image\n");
+
+    write_bmp(image, IMAGE_DIM, IMAGE_DIM, "raycast_gpu_out.bmp");
+
+    printf("Done with program\n");
+    return 0;
 }
