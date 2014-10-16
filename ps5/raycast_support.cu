db3c8b9fc0322577d6bc349948c735ddc2d8a987
x10an14 <x10an14@hotmail.com>
2014-10-16 22:44:27 +0200

    More printfs...

--- ps5/raycast_support.cu
+++ ps5/raycast_support.cu
@@ -86,11 +86,14 @@ void print_properties(){
         printf("Device #%d, Name: %s\n" , (i+1), p.name);
         printf("Compute capability: %d.%d\n", p.major, p.minor);
 
-        printf("Total memory: %zdMiB \nShared memory per block: %zdKiB\n", p.totalGlobalMem/(1024*1024), p.sharedMemPerBlock/1024);
+        printf("Total memory: %zd MiB \nShared memory per thread block in a SM/SMX: %zd KiB\n",
+            p.totalGlobalMem/(1024*1024), p.sharedMemPerBlock/1024);
 
         printf("#Threads per Warp: %d\n", p.warpSize);
 
         printf("Multiprocessor (SM/SMX) count: %d\n", p.multiProcessorCount);
+
+        printf("Max #threads per SM/SMX: %d\n", p.maxThreadsPerMultiProcessor);
 
         printf("Max threads per Blocks: ");
         for (int j = 0; j < 2; ++j){
@@ -104,12 +107,14 @@ void print_properties(){
 
         printf("Max Threads per Block: %d\n", p.maxThreadsPerBlock);
 
-        printf("Are concurrent kernels supported?: %s\n", p.concurrentKernels ? "yes" : "no");
+        printf("Are concurrent kernels supported?: %s\n",
+            p.concurrentKernels ? "yes" : "no");
 
         if (p.asyncEngineCount){
-            printf("GPU can copy %s concurrently host and device while executing a kernel\n", p.asyncEngineCount == 2 ? "both ways between " : "one way between");
+            printf("Device can transfer data %s host and device while executing a kernel\n",
+                p.asyncEngineCount == 2 ? "both ways between" : "one way between");
         } else{
-            printf("GPU cannot transfer data between host/device while a kernel is running\n");
+            printf("Device cannot transfer data between host/device while a kernel is running\n");
         }
 
         printf("\n\n");
